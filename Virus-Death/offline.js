c5293e5ec64258a0c3cbd5c8f6c4f8037c54ed2d
EduardoGape <git.eduardo.leite@gmail.com>
2022-06-08 12:16:33 -0400

    matheus-ck2-maplebear

---
+++ Virus-Death/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1654704127,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/espinhos-sheet0.png",
+		"images/escada-sheet0.png",
+		"images/diamante-sheet0.png",
+		"images/background-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
